ab7a7a997a6b47b163f639628df3101ef520f0d0
Makieva <maeva.oukid@gmail.com>
2024-04-17 16:33:14 +0200

    extraction des donnees excel

--- handleForm.js
+++ handleForm.js
@@ -1,40 +1,42 @@
-export function handleSubmit() {
-    const file = document.getElementById("submitButton");
-    file.addEventListener("change", (event) => {
-      var reader = new FileReader();
-      reader.readAsArrayBuffer(event.target.files[0]);
-      reader.onload = function (event) {
-        var data = new Uint8Array(reader.result);
-        var work_book = XLSX.read(data, { type: "array" });
-        var sheet_name = work_book.SheetNames;
-        var sheet_data = XLSX.utils.sheet_to_json(
-          work_book.Sheets[sheet_name[0]],
-          { header: 1 }
-        );
+export const dataStudent = localStorage.getItem("dataStudent");
+const file = document.getElementById("filepicker");
 
-        if (sheet_data.length > 0) {
-          var table_output = "<table>";
-          for (var row = 0; row < sheet_data.length; row++) {
-            table_output += "<tr>";
-            for (var cell = 0; cell < sheet_data[row].length; cell++) {
-              table_output += "<td>" + sheet_data[row][cell] + "<td>";
-            }
-            table_output += "<td>";
-          }
-          table_output += "</table>";
-          document.getElementById("resultat").innerHTML = table_output;
-        }
-      };
+file.addEventListener("change", (event) => {
+  var reader = new FileReader();
+  reader.readAsArrayBuffer(event.target.files[0]);
+  reader.onload = function (event) {
+    var data = new Uint8Array(reader.result);
+    var work_book = XLSX.read(data, { type: "array" });
+    var sheet_name = work_book.SheetNames;
+    var sheet_data = XLSX.utils.sheet_to_json(work_book.Sheets[sheet_name[0]], {
+      header: 1,
     });
-}
+    var slicedSheetData = sheet_data.slice(1);
+    formatSheet(slicedSheetData);
+  };
+});
 
-export function ecrit(){
-    const test = document.getElementById('voici');
-    test.addEventListener('click', (event) => {
-        console.log('voici');
-    })
-}
+function formatSheet(sheet_data) {
+  var nomColumn = 0;
+  var prenomColumn = 1;
+  var codePostalColumn = 15;
+  var adresseColumn = 17;
+  var villeColumn = 20;
 
-export function er(){
-    console.log('sisis');
+  var filteredData = sheet_data.map((row) => ({
+    nom: row[nomColumn],
+    prenom: row[prenomColumn],
+    codePostal: row[codePostalColumn],
+    adresse: row[adresseColumn],
+    ville: row[villeColumn],
+  }));
+
+  var dataStringify = JSON.stringify(filteredData);
+
+  var concatenatedAdress = sheet_data.map(
+    (row) =>
+      row[adresseColumn] + " " + row[codePostalColumn] + " " + row[villeColumn]
+  );
+
+  localStorage.setItem("dataStudent", dataStringify);
 }
