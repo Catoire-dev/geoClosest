b463ed7a095867946910aac32bbad973daae0d77
Makieva <maeva.oukid@gmail.com>
2024-04-18 14:11:41 +0200

    boucle sur les students pour récupération données

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import { dataStudent } from "./handleForm.js";
+import { get_coordinates } from "./api.js";
+import { get_distance } from "./api.js";
+
+var parsedStudent = JSON.parse(dataStudent);
+
+parsedStudent.forEach((student, index) => {
+  get_coordinates(
+    student.adresse + " " + student.codePostal + " " + student.ville
+  );
+
+  if (index < parsedStudent.length - 1) {
+    get_distance(
+      student.latitude,
+      student.longitude,
+      //   company.latitude,
+      //   company.longitude,
+      48.8398174, //exemple
+      2.353964, //exemple
+      "pedestrian"
+    );
+  }
+});
